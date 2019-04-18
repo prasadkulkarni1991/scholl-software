c9baa319e92c8b71d06b43ef529578bde23dd962
Prasad-Techila <prasad.kulkarni@tehilaservices.com>
2019-04-18 11:25:51 +0530

    test2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,43 +6,51 @@ import { AboutUsComponent } from './about-us/about-us.component';
 import { NgModule } from '@angular/core';
 
 export const route: Routes = [
-    {path: '', redirectTo: '/login', pathMatch: 'full'},
-    {path: 'login', component: LoginComponent},
-    {
-      path: 'dashboard',
-      component: DashboardComponent,
-      children: [
-          {
-            path: '',
-            redirectTo: 'home',
-            pathMatch: 'full'
+  {
+    path: '',
+    redirectTo: '/login',
+    pathMatch: 'full'
+  },
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+  {
+    path: 'dashboard',
+    component: DashboardComponent,
+    children: [
+      {
+        path: '',
+        redirectTo: 'home',
+        pathMatch: 'full'
 
-          },
-          {
-            path: 'home',
-            component: HomeComponent
-          },
-          {
-            path: 'about-us',
-            component: AboutUsComponent
-          },
-          {
-            path: 'student',
-            loadChildren: './student/student.module#StudentModule'
-          },
-          {
-            path: 'employee',
-            loadChildren: './employee/employee.module#EmployeeModule'
-          }
-      ]
-    },
+      },
+      {
+        path: 'home',
+        component: HomeComponent
+      },
+      {
+        path: 'about-us',
+        component: AboutUsComponent
+      },
+      {
+        path: 'student',
+        loadChildren: './student/student.module#StudentModule'
+      },
+      {
+        path: 'employee',
+        loadChildren: './employee/employee.module#EmployeeModule'
+      }
+    ]
+  },
 
-  ];
+];
 
-  @NgModule({
-      imports: [RouterModule.forRoot(route)],
-      exports: [RouterModule]
-  })
 
-  export class AppRoutingModule { }
+@NgModule({
+  imports: [RouterModule.forRoot(route)],
+  exports: [RouterModule]
+})
+
+export class AppRoutingModule { }
 
